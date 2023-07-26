6b63333e268b262c03c385cffb2bf57e7be38e35
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-07-26 12:37:22 +0900

    Login Page
    Login jsx, css

--- TestProject002/demo/frontend/reactt002/src/components/default/Login.jsx
+++ TestProject002/demo/frontend/reactt002/src/components/default/Login.jsx
@@ -14,38 +14,59 @@ const Login = () => {
               <div className={styles.login_contents}>
 
                     <div className={styles.login_contents_tbl}>
-                        <table>
-                                <thead>
+                        <table className={styles.login_contents_tbl_all}>
+                                <thead className={styles.login_contents_tbl_thead}>
                                     <tr>
                                         <th colSpan="2">
                                             Welcome!
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody className={styles.login_contents_tbl_tbody}>
                                     <tr>
-                                        <th> ID </th>
-                                        <td> ID를 입력하세요</td>
+                                        <th className={styles.login_contents_tbl_th}> 
+                                            <div className={styles.login_contents_tbl_th_div}>
+                                                ID 
+                                            </div>
+                                        </th>
+                                        <td className={styles.login_contents_tbl_td}> 
+                                            <div className={styles.login_contents_tbl_td_div}>
+                                                <input type='text' placeholder='ID를 입력하세요' className={styles.login_contents_tbl_td_div_input}></input> 
+                                            </div>
+                                        </td>
                                     </tr>
                                     <tr>
-                                        <th>PW</th>
-                                        <th> PW를 입력하세요</th>
+                                        <th className={styles.login_contents_tbl_th}> 
+                                            <div className={styles.login_contents_tbl_th_div}>
+                                                PW
+                                            </div>
+                                        </th>
+                                        <td className={styles.login_contents_tbl_td}> 
+                                            <div className={styles.login_contents_tbl_td_div}>
+                                                <input type='password' placeholder='PW를 입력하세요' className={styles.login_contents_tbl_td_div_input}></input> 
+                                            </div>
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
                     </div> {/*   end region : login_contents_tbl */}
 
                     <div className={styles.login_contents_btn_area}>
-                        로그인
-                        <div>
-                            ID 찾기
-                            PW 찾기
+                        <input type='button' className={styles.login_contents_btn} value='로그인'></input>
+                        <div className={styles.login_contents_btn_find}>
+                            <div className={styles.login_contents_btn_find_div_left}>
+                            <input type='button' className={styles.login_contents_btn_find_div_left_btn} value='ID 찾기'></input>
+                            </div>
+                            <div className={styles.login_contents_btn_find_div_right}>
+                            <input type='button' className={styles.login_contents_btn_find_div_right_btn} value='PW 찾기'></input>
+                            </div>
                         </div>
                         <hr className={styles.login_contents_hr}></hr>
                     </div>
 
-                    <div>
-                        계정이 없으신가요? 회원가입
+                    <div className={styles.login_contents_btn_find_div_signup}>
+                        계정이 없으신가요? 
+                    <input type='button' className={styles.login_contents_btn_find_div_signup_btn} value='회원가입'></input>
                     </div>
                     
                 </div> {/*   end region : login_contents */}
