b2d5743dc5b116098e43860f66a856ccde6a1c28
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-07-28 12:22:08 +0900

    Code Api
    Code Api

---
+++ TestProject002/demo/src/main/java/com/example/demo/CodeController.java
@@ -0,0 +1,69 @@
+package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.ibatis.session.SqlSession;
+
+import com.example.mapper.CodeMapper;
+import com.example.model.CodeModel;
+import com.example.entity.CodeEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.sql.*;
+
+
+@RestController
+public class CodeController {
+    
+    @Autowired
+    private SqlSession sqlSession;
+
+    /// 설명 : 코드 1건 (파라미터 : 코드 ID) 반환 API
+    @GetMapping("/api/code/select_code_entity")
+    public HashMap<String, Object> SelectCodeEntity(String Code_ID)  throws SQLException, ClassNotFoundException
+    {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        CodeModel request = new CodeModel();
+        if(Code_ID == "" || Code_ID == null)
+        {
+            Code_ID = "RECIPE";
+        }
+        request.Code_ID = Code_ID;
+        
+        CodeEntity resultCodeEntity = new CodeEntity();
+
+        CodeMapper mapper = sqlSession.getMapper(CodeMapper.class);
+        resultCodeEntity = mapper.selectCodeEntity(request);
+
+        result.put("CodeEntity", resultCodeEntity);    
+        return result;
+    }
+
+    /// 설명 : 코드 여러건 (파라미터 : 부모코드 ID) 반환 API
+    @GetMapping("/api/code/select_code_entity_list")
+    public HashMap<String, Object> SelectCodeEntityList(String Parent_Code_ID)  throws SQLException, ClassNotFoundException
+    {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        CodeModel request = new CodeModel();
+        if(Parent_Code_ID == "" || Parent_Code_ID == null)
+        {
+            Parent_Code_ID = "RECIPE";
+        }
+        request.Parent_Code_ID = Parent_Code_ID;
+        
+        List<CodeEntity> resultCodeEntityList = new ArrayList<CodeEntity>();
+
+        CodeMapper mapper = sqlSession.getMapper(CodeMapper.class);
+        resultCodeEntityList = mapper.selectCodeEntityList(request);
+
+        result.put("CodeEntityList", resultCodeEntityList);    
+        return result;
+    }
+
+    
+
+}
--- TestProject002/demo/src/main/java/com/example/mapper/CodeMapper.java
+++ TestProject002/demo/src/main/java/com/example/mapper/CodeMapper.java
@@ -1,9 +1,26 @@
 package com.example.mapper;
 import com.example.model.CodeModel;
 
+import java.util.ArrayList;
+
+import com.example.entity.CodeEntity;
+
 
 
 
 public interface CodeMapper {
-    public CodeModel selectCodeNm(CodeModel codeModel);
+
+    // 단일 코드 반환 : UP_CMM_Code_S
+    public CodeEntity selectCodeEntity(CodeModel codeModel);
+
+    // 복수 코드 반환 : UP_CMM_Code_L
+    public ArrayList<CodeEntity> selectCodeEntityList(CodeModel codeModel);
+
+    // 코드 생성 : UP_CMM_Code_C
+    public int createCodeEntity(CodeModel codeModel);
+
+    // 코드 수정 : UP_CMM_Code_U
+    public int ureateCodeEntity(CodeModel codeModel);
+
+
 }
